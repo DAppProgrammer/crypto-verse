82f3cdfb41eb6e08babe7ef2ce63c1b1f3c588e0
Paramjeet Singh <dapp.programmer@gmail.com>
2021-12-29 00:00:16 +0530

    Layout design related changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News  } from './components';
+import './App.css';
 
 const app = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path='/' element={<Homepage/>} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies/>} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+
+            </div>
         </div>
     )
 }
