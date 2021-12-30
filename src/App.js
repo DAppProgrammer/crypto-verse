849de59d82c9e77b5204372aa2e0ff77fe4e5c19
Paramjeet Singh <dapp.programmer@gmail.com>
2021-12-30 19:49:47 +0530

    Created reducer for cryptoApi

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Router, Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News  } from './components';
 import './App.css';
+import { whileStatement } from '@babel/types';
 
 const app = () => {
     return (
@@ -15,16 +16,24 @@ const app = () => {
                     <div className='routes'>
                         <Routes>
                             <Route path='/' element={<Homepage/>} />
-                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/exchanges' element={<Exchanges/>} />
                             <Route path='/cryptocurrencies' element={<Cryptocurrencies/>} />
-                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
-                            <Route path='/news' element={<News />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails/>} />
+                            <Route path='/news' element={<News/>} />
                         </Routes>
                     </div>
                 </Layout>
-            </div>
             <div className='footer'>
-
+                <Typography.Title level={5} style={{color:'white', textAlign:'center'}} >
+                    Cryptoverse <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
             </div>
         </div>
     )
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '8919533329msh3262273c688cf52p1dae51jsn8d1489edb45c'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
