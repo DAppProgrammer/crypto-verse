7696f44b061a1bdf1fdcbf302ae49dad76c4dd10
Paramjeet Singh <dapp.programmer@gmail.com>
2022-01-04 22:45:25 +0530

    Created CryptoDetails component and its API

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Select, Typography, Row, Col, Avatar, Card} from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsAPI';
+import { useGetCryptosQuery } from '../services/cryptoAPI';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
 const demoImage = "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 
-const News = (simplified) => {
-    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12});
+const News = ({simplified}) => {
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12});
+    const { data } = useGetCryptosQuery(100);
     // console.log(cryptoNews);
     if(!cryptoNews?.value) return 'Loading...';
 
     return (
         <Row gutter={[24,24]}>
+            {
+                !simplified &&
+                (
+                    <Col span={24}>
+                        <Select
+                            showSearch
+                            className="select-news"
+                            placeholder="Select a Crypto"
+                            optionFilterProp="children"
+                            onChange={(value) => setNewsCategory(value)}
+                            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                        >
+                            <Option value="Cryptocurrency" key="Cryptocurrency" >Cryptocurrency</Option>
+                            {data?.data?.coins.map((coin) => <Option value={coin.name} key={coin.name} >{coin.name} </Option> )}
+                        </Select>
+                    </Col>
+                )
+            }
             {cryptoNews.value.map((news,i) => (
                 <Col xs={24} sm={12} lg={8} >
-                    <Card hoverable className="news-card">
+                    <Card hoverable className="news-card" key={i}>
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,26 +1,26 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    // 'x-rapidapi-key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA'
-    'x-rapidapi-key': '8919533329msh3262273c688cf52p1dae51jsn8d1489edb45c'
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  // 'x-rapidapi-key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA'
+  "x-rapidapi-key": "8919533329msh3262273c688cf52p1dae51jsn8d1489edb45c"
+};
 
-}
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
-
-const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`),
-        })
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`)
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`coin/${coinId}`)
     })
+  })
 });
 
-export const {
-    useGetCryptosQuery,
-} = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
