ef4bdcf135451434253a358ba4c04a470ae2f009
Paramjeet Singh <dapp.programmer@gmail.com>
2022-01-05 14:13:03 +0530

    Implemented spin loader to all pages

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoAPI'
 import { Cryptocurrencies, News } from '.'
+import Loader from './Loader'
 
 const { Title } = Typography
 
@@ -12,7 +13,7 @@ const Homepage = () => {
     const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
-    if(isFetching) return 'loading...';
+    if(isFetching) return <Loader/>
 
     return (
         <>
